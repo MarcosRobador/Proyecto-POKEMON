99ceea329864dabba95c4128ad94bc4fa670ed3b
Marcos <Marcoscabet@gmail.com>
2024-01-14 20:01:57 +0100

    conexion bbdd

---
+++ Conexion/conexion.php
@@ -0,0 +1,11 @@
+<?php
+
+$host = 'localhost'; 
+$usuario = 'root';
+$contrasena = 'root';
+$base_datos = 'banco';
+
+// Conexión a la base de datos
+$conexion = new mysqli($host, $usuario, $contrasena, $base_datos);
+
+?>
