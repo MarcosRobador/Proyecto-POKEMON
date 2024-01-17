a03cadcf9de0f56fc234d458ba594ca8b7e0c169
Marcos <Marcoscabet@gmail.com>
2024-01-17 18:56:32 +0100

    Validacion de login.
    En datos-register.php he añadido codigo para comprobar que el correo a la hora de registrarte sea diferente a los registrados anteriormente
    
    login y register finalizados, con sus validaciones

--- login-register/datos-register.php
+++ login-register/datos-register.php
@@ -1,7 +1,6 @@
 <?php
 include '../Conexion/conexion.php'; 
 
-
 // Verifica si el formulario fue enviado
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Recopilación y saneamiento de los datos del formulario
@@ -10,26 +9,45 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $password = $conexion->real_escape_string($_POST['password']);
     $apellido = $conexion->real_escape_string($_POST['apellido']);
 
-    // Encripta la contraseña
-    $password_encriptada = password_hash($password, PASSWORD_DEFAULT);
+    // Validar el formato del email
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "Formato de correo electrónico inválido.";
+        exit;
+    }
 
-    // Consulta con el IBAN
-    $consulta = "INSERT INTO usuarios (nombre, email, password, apellido) VALUES ('$nombre', '$email', '$password_encriptada', '$apellido')";
+    // Comprobar si el email ya está registrado
+    $stmt = $conexion->prepare("SELECT COUNT(*) FROM usuarios WHERE email = ?");
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $stmt->bind_result($count);
+    $stmt->fetch();
+    $stmt->close();
 
-    // Ejecuta la consulta de inserción en la base de datos
-    if (isset($consulta) && $consulta) {
-        if ($conexion->query($consulta) === TRUE) {
-            session_start();
-            $_SESSION['nombre'] = $nombre;
-            header('Location: login.html');
-            exit;
-        } else {
-            echo "Error: " . $conexion->error;
-        }
+    if ($count > 0) {
+        echo "Este correo electrónico ya está registrado.<br>";
+        echo "<button onclick=\"location.href='register.html'\">Registrar de Nuevo</button>";
+        exit;
     } else {
-        echo "Consulta SQL no definida o vacía.";
+        // El email es único, proceder con el registro
+        $password_encriptada = password_hash($password, PASSWORD_DEFAULT);
+        $consulta = "INSERT INTO usuarios (nombre, email, password, apellido) VALUES ('$nombre', '$email', '$password_encriptada', '$apellido')";
+
+        // Ejecuta la consulta de inserción en la base de datos
+        if (isset($consulta) && $consulta) {
+            if ($conexion->query($consulta) === TRUE) {
+                session_start();
+                $_SESSION['nombre'] = $nombre;
+                header('Location: login.html');
+                exit;
+            } else {
+                echo "Error: " . $conexion->error;
+            }
+        } else {
+            echo "Consulta SQL no definida o vacía.";
+        }
     }
 
     $conexion->close();
 }
+
 ?>
