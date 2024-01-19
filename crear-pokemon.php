39524c7b9f4c089b7b166e723bfb00e13f5cbb0c
Marcos <Marcoscabet@gmail.com>
2024-01-19 21:29:10 +0100

    FIN

--- crear-pokemon.php
+++ crear-pokemon.php
@@ -12,14 +12,14 @@
     
   <header>
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="003pkms.php">Pokémon</a>
+        <a class="navbar-brand" href="index.php">Pokémon</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbar">
           <ul class="navbar-nav">
             <li class="nav-item active">
-              <a class="nav-link" href="003pkms.php">Inicio</a>
+              <a class="nav-link" href="index.php">Inicio</a>
             </li>
           </ul>
         </div>
